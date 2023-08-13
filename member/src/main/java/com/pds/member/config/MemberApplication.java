cdaec1ba2ec3f3ffaee44b0f151ec6495e8e94eb
pdsong <songpeidong@gmail.com>
2023-08-14 02:04:55 +0800

    新建模块member，修改模块pom，移除父级src文件夹

---
+++ member/src/main/java/com/pds/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.pds.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.pds")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
--- src/main/java/com/pds/train/TestController.java
+++
@@ -1,2 +0,0 @@
-package com.pds.train;public class TestController {
-}
