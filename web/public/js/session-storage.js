2333810aa33e5d69e85be7b7be58220c08e67a7b
pdsong <songpeidong@gmail.com>
2024-08-18 03:04:01 +0800

    add vuex使用h5 session缓存解决浏览器刷新丢失Member数据问题

---
+++ web/public/js/session-storage.js
@@ -0,0 +1,18 @@
+SessionStorage = {
+    get: function (key) {
+        //h5提供的缓存 原生只支持字符串 所以添加了set remove clearall封装
+        var v = sessionStorage.getItem(key);
+        if (v && typeof(v) !== "undefined" && v !== "undefined") {
+            return JSON.parse(v);
+        }
+    },
+    set: function (key, data) {
+        sessionStorage.setItem(key, JSON.stringify(data));
+    },
+    remove: function (key) {
+        sessionStorage.removeItem(key);
+    },
+    clearAll: function () {
+        sessionStorage.clear();
+    }
+};
