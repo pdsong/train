f1d93a967cba27df81c42c092871d7509c7f0653
pdsong <songpeidong@gmail.com>
2024-08-27 00:42:44 +0800

    添加 火车车站管理 后端实现;

---
+++ business/src/main/java/com/pds/business/mapper/TrainStationMapper.java
@@ -0,0 +1,33 @@
+package com.pds.business.mapper;
+
+
+
+import com.pds.business.domain.TrainStation;
+import com.pds.business.domain.TrainStationExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TrainStationMapper {
+    long countByExample(TrainStationExample example);
+
+    int deleteByExample(TrainStationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TrainStation record);
+
+    int insertSelective(TrainStation record);
+
+    List<TrainStation> selectByExample(TrainStationExample example);
+
+    TrainStation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TrainStation record, @Param("example") TrainStationExample example);
+
+    int updateByExample(@Param("record") TrainStation record, @Param("example") TrainStationExample example);
+
+    int updateByPrimaryKeySelective(TrainStation record);
+
+    int updateByPrimaryKey(TrainStation record);
+}
