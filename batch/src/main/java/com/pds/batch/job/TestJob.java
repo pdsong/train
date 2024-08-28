da00bb09e0154c0281db1a852875d4f3c0a2ec18
pdsong <songpeidong@gmail.com>
2024-08-28 22:20:34 +0800

    禁止任务并发执行;注掉springboot自带定时任务;

--- batch/src/main/java/com/pds/batch/job/TestJob.java
+++ batch/src/main/java/com/pds/batch/job/TestJob.java
@@ -1,13 +1,24 @@
 package com.pds.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+
+//添加注解 禁止并发执行 上一个执行完 开始下一个
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJOB   test!");
+        System.out.println("TestJOB   test start!");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJOB   test end!");
+
     }
 }
